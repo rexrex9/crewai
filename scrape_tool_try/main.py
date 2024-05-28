0d218ce9243572bc451c020073ab184ad0f1a2d3
rexrex9 <547102444@qq.com>
2024-05-28 15:29:46 +0800

    init

--- scrape_tool_try/main.py
+++ scrape_tool_try/main.py
@@ -4,13 +4,17 @@ from scrape_tool_try.agents import links_agent as la, detail_agent as da
 from datas import filepaths as fp
 import json
 def scray_try():
-    num = 5
+    num = 10
     ls = la.get_links(num)
     for l in ls:
-        d = da.get_game_detail(l)
-        p = os.path.join(fp.GAME_DIR, d['name']+".json")
-        with open(p, 'w+') as f:
-            f.write(json.dumps(d, indent=4))
+        try:
+            d = da.get_game_detail(l)
+            p = os.path.join(fp.GAME_DIR, d['name']+".json")
+            with open(p, 'w+') as f:
+                f.write(json.dumps(d, indent=4))
+        except Exception as e:
+            print("error")
+            continue
 
 if __name__ == '__main__':
     scray_try()
