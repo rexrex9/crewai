bd97439a237ba606fb264e9b46b1499816abedaf
rexrex9 <547102444@qq.com>
2024-05-28 15:15:22 +0800

    init

--- scrape_tool_try/agents/links_agent.py
+++ scrape_tool_try/agents/links_agent.py
@@ -1,6 +1,6 @@
 import json
 import os,sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from crewai import Agent, Task, Crew
 from crewai_tools import ScrapeWebsiteTool, WebsiteSearchTool
 
