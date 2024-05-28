b208f2743c606cc3d4dcf27508577808e1c79b3c
rexrex9 <547102444@qq.com>
2024-05-28 10:58:26 +0800

    init

--- finished/two.py
+++ finished/two.py
@@ -11,8 +11,11 @@ overall_agent = Agent(
     role="找GameFi详细页面谅解的助理",
 	goal="找到GameFi游戏的详细页面连接",
 	backstory=(
-        "https://playtoearn.com/blockchaingame/+游戏名是一个GameFi游戏详细页面连接,"
-        "所以仅需找到游戏名组合成的这样的连接返回用户即可。"
+        'https://playtoearn.com/blockchaingame/+游戏名是一个GameFi游戏详细页面连接,'
+        '所以仅需找到游戏名组合成的这样的连接返回用户即可。请注意，这些链接中的游戏名中的空格需'
+        '要替换成"-"。例如，如果游戏名是"The New Order"，'
+        '那么详细页面的链接就是"https://playtoearn.com/blockchaingame/The-New-Order",'
+        '请确保返回的链接是有效的。'
 	),
 	allow_delegation=False,
 	verbose=True
@@ -22,7 +25,7 @@ overall_scrape_tool = ScrapeWebsiteTool(website_url=url)
 
 find_game_task = Task(
     description=("用户{customer}需要找到各个游戏的详细页面连接"),
-    expected_output=("找到GameFi游戏详细页面连接,输出一个列表给用户{customer}"),
+    expected_output=("找到GameFi游戏详细页面连接,输出一个列表给用户{customer},数量为{num}个"),
 	tools=[overall_scrape_tool],
     agent=overall_agent,
 )
@@ -37,6 +40,7 @@ crew = Crew(
 inputs = {
     "customer": "张三",
     "inquiry": "给我找10个GameFi游戏的详细页面连接",
+    "num": 10
 }
 result = crew.kickoff(inputs=inputs)
 
