e95c1a66f8ba65ba2026d7c715beee6ddbe12b12
rexrex9 <547102444@qq.com>
2024-06-05 16:18:29 +0800

    init

--- scrape_tool_try/conn/t.py
+++ scrape_tool_try/conn/t.py
@@ -1,10 +1,11 @@
-import os
-from dotenv import load_dotenv
+import os,sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
-load_dotenv()
+
 
 api_key = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
