bb79af027bf715f915c19f4da1bec96df805cc21
rexrex9 <547102444@qq.com>
2024-06-06 16:14:09 +0800

    init

--- scrape_tool_try/conn/t.py
+++ scrape_tool_try/conn/t.py
@@ -2,23 +2,10 @@ import os,sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-# from langchain.prompts import PromptTemplate
-# from langchain.chains import LLMChain
 from crewai import Agent, Task, Crew
-if "OPENAI_API_KEY" in os.environ:
-    del os.environ["OPENAI_API_KEY"]
+
 api_key = os.getenv("GOOGLE_API_KEY")
-llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
-
-# prompt_template = PromptTemplate.from_template("Write a short story about {topic}.")
-# llm_chain = LLMChain(llm=llm, prompt=prompt_template)
-#
-# if __name__ == "__main__":
-#     topic = "a cat"
-#     response = llm_chain.run(topic=topic)
-#     print(response)
-
-
+llm = ChatGoogleGenerativeAI(model="gemini-flash-1.5", google_api_key=api_key)
 
 agent = Agent(
     role="情感分析专家",
@@ -27,22 +14,16 @@ agent = Agent(
     llm=llm
 )
 
-
 task = Task(
     description="根据用户的{query}，找到他的情绪倾向",
     expected_output="用户的情绪倾向",
     agent=agent,
-    #llm=llm
 )
-
 
 crew = Crew(
     agents=[agent],
     tasks=[task],
     verbose=2,
-    #memory=True,
-    #manager_llm=llm,
-    #llm=llm
 )
 
 inputs = {
