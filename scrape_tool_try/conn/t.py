1085f30a652de22da795bb8c32ef805909945992
rexrex9 <547102444@qq.com>
2024-06-06 14:06:59 +0800

    init

--- scrape_tool_try/conn/t.py
+++ scrape_tool_try/conn/t.py
@@ -5,7 +5,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain.prompts import PromptTemplate
 # from langchain.chains import LLMChain
 from crewai import Agent, Task, Crew
-os.environ["OPENAI_API_KEY"] = "NA"
+if "OPENAI_API_KEY" in os.environ:
+    del os.environ["OPENAI_API_KEY"]
 api_key = os.getenv("GOOGLE_API_KEY")
 llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
 
