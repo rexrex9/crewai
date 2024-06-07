73552e6b81fa6d5046927ae9dc3e269cb0329874
rexrex9 <547102444@qq.com>
2024-06-07 14:20:43 +0800

    init

--- scrape_tool_try/agents/basic.py
+++ scrape_tool_try/agents/basic.py
@@ -1,28 +1,28 @@
 import os,sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
-from langchain_google_genai import ChatGoogleGenerativeAI
-api_key = os.getenv("GOOGLE_API_KEY")
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest", google_api_key=api_key)
-
 from crewai import Agent, Task, Crew
+from crewai_tools import ScrapeWebsiteTool, WebsiteSearchTool, PDFSearchTool
+from crewai import Process
+
 agent = Agent(
     role="情感分析专家",
     goal="研究用户的{query}，找到他的情绪倾向",
     backstory="你是一位情感分析专家，你的任务是根据用户的{query}，找到他的情绪倾向。",
-    llm=llm
 )
+
 
 task = Task(
     description="根据用户的{query}，找到他的情绪倾向",
     expected_output="用户的情绪倾向",
-    agent=agent,
+    agent=agent
 )
+
 
 crew = Crew(
     agents=[agent],
     tasks=[task],
     verbose=2,
+    memory=True
 )
 
 inputs = {
@@ -31,4 +31,4 @@ inputs = {
 
 result = crew.kickoff(inputs=inputs)
 
-
+print(result)
--- tutor1_basic/basic.py
+++ tutor1_basic/basic.py
@@ -1,32 +1,43 @@
 import os,sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from crewai import Agent, Task, Crew
-from crewai_tools import ScrapeWebsiteTool, WebsiteSearchTool, PDFSearchTool
-from crewai import Process
 
-agent = Agent(
-    role="情感分析专家",
-    goal="研究用户的{query}，找到他的情绪倾向",
-    backstory="你是一位情感分析专家，你的任务是根据用户的{query}，找到他的情绪倾向。",
+gamefi_search_agent = Agent(
+    role="GameFi Search Assistant",
+    goal="Find the GameFi game's detailed according to the user's query,which is {query}",
+    backstory="You are a GameFi Search Assistant, your task is to find the detailed page"
+              " of the GameFi game according to the user's query. GameFi is the blockchain game"
 )
 
+gamefi_recommend_agent = Agent(
+    role="GameFi Recommend Assistant",
+    goal="Recommend the GameFi game according to the user's query,which is {query}",
+    backstory="You are a GameFi Recommend Assistant, your task is to recommend the GameFi game"
+              " according to the user's query. GameFi is the blockchain game"
+)
 
-task = Task(
-    description="根据用户的{query}，找到他的情绪倾向",
-    expected_output="用户的情绪倾向",
-    agent=agent
+task_search = Task(
+    description="find GameFi games according to {query}",
+    expected_output="games' details",
+    agent=gamefi_search_agent
+)
+
+task_recommend = Task(
+    description="recommend GameFi games according to {query}",
+    expected_output="games' recommendation",
+    agent=gamefi_recommend_agent
 )
 
 
 crew = Crew(
-    agents=[agent],
-    tasks=[task],
+    agents=[gamefi_search_agent, gamefi_recommend_agent],
+    tasks=[task_search, task_recommend],
     verbose=2,
     memory=True
 )
 
 inputs = {
-    "query": "我很开心"
+    "query": "I need a fps gameFi game"
 }
 
 result = crew.kickoff(inputs=inputs)
